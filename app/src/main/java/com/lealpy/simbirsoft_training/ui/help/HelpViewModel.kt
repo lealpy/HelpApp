0e56f4dafb0c611e4d67da0020b8d3d53856831b
lealpy <lealp@mail.ru>
2021-12-20 12:44:37 +0300

    added jsons

--- app/src/main/java/com/lealpy/simbirsoft_training/ui/help/HelpViewModel.kt
+++ app/src/main/java/com/lealpy/simbirsoft_training/ui/help/HelpViewModel.kt
@@ -1,23 +1,29 @@
 package com.lealpy.simbirsoft_training.ui.help
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.lealpy.simbirsoft_training.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class HelpViewModel(application: Application): AndroidViewModel(application) {
 
-    private val _helpItems = MutableLiveData(
-        listOf(
-            HelpItem(0, R.drawable.child, application.getString(R.string.help_category_children)),
-            HelpItem(1, R.drawable.adult, application.getString(R.string.help_category_adults)),
-            HelpItem(0, R.drawable.elderly, application.getString(R.string.help_category_elderly)),
-            HelpItem(0, R.drawable.animal, application.getString(R.string.help_category_animals)),
-            HelpItem(0, R.drawable.event, application.getString(R.string.help_category_events))
-        )
-    )
+    private val jsonFileString = getJsonDataFromAsset(application, HELP_ITEMS_JSON_FILE_NAME)
+    private val gson = Gson()
+    private val itemTypes = object : TypeToken<List<HelpItem>>() {}.type
+    private val helpItemsFromJson : List<HelpItem> = gson.fromJson(jsonFileString, itemTypes)
 
+    private val _helpItems = MutableLiveData( helpItemsFromJson )
     val helpItems: LiveData<List<HelpItem>> = _helpItems
+
+    private fun getJsonDataFromAsset(context: Context, fileName: String): String {
+        return context.assets.open(fileName).bufferedReader().use { it.readText() }
+    }
+
+    companion object {
+        private const val HELP_ITEMS_JSON_FILE_NAME = "help_items.json"
+    }
 
 }
--- app/src/main/java/com/lealpy/simbirsoft_training/ui/news/NewsViewModel.kt
+++ app/src/main/java/com/lealpy/simbirsoft_training/ui/news/NewsViewModel.kt
@@ -1,53 +1,21 @@
 package com.lealpy.simbirsoft_training.ui.news
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.lealpy.simbirsoft_training.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class NewsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val newsList = listOf(
-        NewsItem(
-            id = 0,
-            image = R.drawable.news_item_1_image,
-            title = application.getString(R.string.news_item_title_1),
-            abbreviatedText = application.getString(R.string.news_item_1_abbreviated_text),
-            date = application.getString(R.string.news_item_1_date),
-            fundName = application.getString(R.string.news_item_1_fund_name),
-            address = application.getString(R.string.news_item_1_address),
-            phone = application.getString(R.string.news_item_1_phone),
-            image2 = R.drawable.news_item_1_image_2,
-            image3 = R.drawable.news_item_1_image_3,
-            fullText = application.getString(R.string.news_item_1_full_text),
-            isChildrenCategory = true,
-            isAdultsCategory = false,
-            isElderlyCategory = false,
-            isAnimalsCategory = false,
-            isEventsCategory = false
-        ),
-        NewsItem(
-            id = 1,
-            image = R.drawable.news_item_2_image,
-            title = application.getString(R.string.news_item_2_title),
-            abbreviatedText = application.getString(R.string.news_item_2_text),
-            date = application.getString(R.string.news_item_2_date),
-            fundName = application.getString(R.string.news_item_2_fund_name),
-            address = application.getString(R.string.news_item_2_address),
-            phone = application.getString(R.string.news_item_2_phone),
-            image2 = R.drawable.news_item_1_image_2,
-            image3 = R.drawable.news_item_1_image_3,
-            fullText = application.getString(R.string.news_item_2_full_text),
-            isChildrenCategory = true,
-            isAdultsCategory = false,
-            isElderlyCategory = false,
-            isAnimalsCategory = false,
-            isEventsCategory = true
-        )
-    )
+    private val jsonFileString = getJsonDataFromAsset(application, NEWS_ITEMS_JSON_FILE_NAME)
+    private val gson = Gson()
+    private val itemTypes = object : TypeToken<List<NewsItem>>() {}.type
+    private val newsItemsFromJson : List<NewsItem> = gson.fromJson(jsonFileString, itemTypes)
 
-    private val _newsItems = MutableLiveData(newsList)
+    private val _newsItems = MutableLiveData( newsItemsFromJson )
     val newsItems: LiveData<List<NewsItem>> = _newsItems
 
     private val _isChildrenChecked = MutableLiveData(true)
@@ -65,6 +33,10 @@ class NewsViewModel(application: Application) : AndroidViewModel(application) {
     private val _isEventsChecked = MutableLiveData(true)
     val isEventsChecked: LiveData<Boolean> = _isEventsChecked
 
+    private fun getJsonDataFromAsset(context: Context, fileName: String): String {
+        return context.assets.open(fileName).bufferedReader().use { it.readText() }
+    }
+
     fun applyFilters(
         isChildrenChecked: Boolean,
         isAdultsChecked: Boolean,
@@ -73,23 +45,23 @@ class NewsViewModel(application: Application) : AndroidViewModel(application) {
         isEventsChecked: Boolean
     ) {
         val childrenCategoryList = if(isChildrenChecked) {
-            newsList.filter { it.isChildrenCategory }
+            newsItemsFromJson.filter { it.isChildrenCategory }
         } else emptyList()
 
         val adultCategoryList = if(isAdultsChecked) {
-            newsList.filter { it.isAdultsCategory }
+            newsItemsFromJson.filter { it.isAdultsCategory }
         } else emptyList()
 
         val elderlyCategoryList = if(isElderlyChecked) {
-            newsList.filter { it.isElderlyCategory }
+            newsItemsFromJson.filter { it.isElderlyCategory }
         } else emptyList()
 
         val animalsCategoryList = if(isAnimalsChecked) {
-            newsList.filter { it.isAnimalsCategory }
+            newsItemsFromJson.filter { it.isAnimalsCategory }
         } else emptyList()
 
         val eventsCategoryList = if(isEventsChecked) {
-            newsList.filter { it.isEventsCategory }
+            newsItemsFromJson.filter { it.isEventsCategory }
         } else emptyList()
 
         val filteredNewsSet = mutableSetOf<NewsItem>()
@@ -107,6 +79,10 @@ class NewsViewModel(application: Application) : AndroidViewModel(application) {
         _isElderlyChecked.value = isElderlyChecked
         _isAnimalsChecked.value = isAnimalsChecked
         _isEventsChecked.value = isEventsChecked
+    }
+
+    companion object {
+        private const val NEWS_ITEMS_JSON_FILE_NAME = "news_items.json"
     }
 
 }
